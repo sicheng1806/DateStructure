01ddebec094b20c0e41472fe4890c4fbe846f5eb
sicheng1806 <sicheng1806@outlook.com>
2023-12-15 22:36:05 +0800

    顺序表

---
+++ hello_c/myfunctions.c
@@ -0,0 +1,11 @@
+// myfunctions.c
+
+#include "myfunctions.h"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
---
+++ hello_c/myfunctions.h
@@ -0,0 +1,9 @@
+// myfunctions.h
+
+#ifndef MYFUNCTIONS_H
+#define MYFUNCTIONS_H
+
+int add(int a, int b);
+int subtract(int a, int b);
+
+#endif
